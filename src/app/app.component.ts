0cba82a128840207e868507f03be4b8e2643fe18
Gabbo <gabriel.ruiz@syloper.com>
2023-09-27 16:42:10 -0300

    feat: :sparkles: use environment variables, show values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngx-env-example';
+  version = import.meta.env.NG_APP_VERSION;
+  exampleText = import.meta.env.NG_APP_EXAMPLE_TEXT;
 }
--- src/env.d.ts
+++ src/env.d.ts
@@ -9,6 +9,8 @@ interface ImportMetaEnv {
    */
   readonly NG_APP_ENV: string;
   // Add your environment variables below
+  readonly NG_APP_VERSION: string;
+  readonly NG_APP_EXAMPLE_TEXT: string;
   // readonly NG_APP_API_URL: string;
   [key: string]: any;
 }
